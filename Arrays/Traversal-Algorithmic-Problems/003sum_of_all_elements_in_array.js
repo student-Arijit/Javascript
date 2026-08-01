767a232cd4cf552c2bbb74a1fb6bcfffcd43b08c
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2026-08-01 09:29:20 +0530

    Refactor sum calculation using reduce method

--- Arrays/Traversal-Algorithmic-Problems/003sum_of_all_elements_in_array.js
+++ Arrays/Traversal-Algorithmic-Problems/003sum_of_all_elements_in_array.js
@@ -1,10 +1,7 @@
 //Compute the sum of all elements.
 
-let arr = [1, 8, 6, 5, 5, 10, 3, 4];
+let arr = [10, 25, 82, 50, 90, 25, 56, 53];
 
-let sum = 0;
-arr.forEach((num) => {
-    sum += num;
-});
+const sum = arr.reduce((sum, current) => sum + current, 0)
 
-console.log("Sum = " + sum);
+console.log(sum)
